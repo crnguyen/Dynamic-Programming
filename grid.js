1be378402eb21b1f0434394e5bbe9d6c423f124c
Cristina Nguyen <c.nguyen0994@gmail.com>
2020-12-16 22:41:14 -0800

    add notes to grid traveler

---
+++ grid.js
@@ -0,0 +1,8 @@
+// say that you are a traveler on a 2D CanvasGradient. you begin 
+// in the top-left corner and your goal is to travel to the bottom right
+// corner. you may only move down or right.
+// in how many ways can you travel to the goal on
+// a grid with dimensions m* n?
+
+//write a function gridtraveler(m,n) that calculates this
+//gridTraveler(2,3) -> 3
